48d259953e93e057d57779f5272ef4383052dc9d
e-bsaid1 <112497602+e-bsaid1@users.noreply.github.com>
2022-09-29 12:36:00 +0200

    Ajout des commentaires

--- Dinner_Guests.py
+++ Dinner_Guests.py
@@ -1,3 +1,4 @@
+#Créer notre répertoire 
 repertoire = ['A','C','D']
 
 print(f"Hello {repertoire[0]}, I invite you,{repertoire[1]} and {repertoire[2]} to diner")
@@ -6,16 +7,18 @@ print(f"Hello {repertoire[1]}, I invite you, {repertoire[0]} and {repertoire[2]}
 
 print(f"Hello {repertoire[2]}, I invite you, {repertoire[1]} and {repertoire[0]} to diner. So we will be {len(repertoire)} !")
 
+#Enlève le contact C de la liste des invités
 not_present = 'C';
 repertoire.remove(not_present);
 print(f"{not_present} cannot come this day");
 print(len(repertoire));
-repertoire.append('E');
 
+#Ajoute le contact E à la liste des invités
+repertoire.append('E');
 print(f"Consequently, I add {repertoire[2]} in place of him")
 
+# Ajoute au premier, quatrième emplacement du répertoire 
 print(f"I found a bigger dinner table so the list of invits are :")
-
 new_invit_1 = repertoire.insert(0, 'H')
 new_invit_2 = repertoire.insert(3,'I')
 new_invit_3 = repertoire.append('J')
@@ -25,6 +28,7 @@ print(f"{repertoire[0]},{repertoire[1]},{repertoire[2]},{repertoire[3]},{reperto
 print("Finally, my new dinner table can't arrive in time for the dinner.");
 print(f"So, we will be {len(repertoire)}");
 
+#Supprime H,I,E,J du répertoire des invités
 cannot_invit_1 = repertoire.pop(0)
 cannot_invit_2 = repertoire.pop(4)
 cannot_invit_3 = repertoire.pop(2)
@@ -34,6 +38,9 @@ print(f"I'm really apologize for the troubles, {cannot_invit_1}, {cannot_invit_2
 
 print(f"So {repertoire[0]} and {repertoire[1]} can come")
 print(f"So, we will be {len(repertoire)}")
+
+#Supprime A et D pour une autre fois ! 
 del repertoire[0] 
 del repertoire[0]
+
 
